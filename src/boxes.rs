c2f82372baecff52421aae50af95706793a883d8
Braden Carlson <bradenjcarlson@live.com>
2026-08-22 08:36:46 -0700

    Messages for boxes
    Added success messages for all box operations. This does make the
    creation process a bit more verbose, but I think that that is ok.

--- src/boxes.rs
+++ src/boxes.rs
@@ -67,14 +67,18 @@ fn list_boxes() -> Result<(), ZkError> {
                 match entry {
                     Ok(e) => {
                         let path = e.path();
+                        if !path.is_dir() {
+                            continue;
+                        }
                         if path == current {
                             pre = "->";
                         }
-                        if path.is_dir() {
-                            if let Some(name) = path.file_name() {
-                                if let Some(dir_name) = name.to_str() {
-                                        println!("{}{}", pre, dir_name);
-                                }
+                        if is_tracked(&path)? == false {
+                            continue;
+                        }
+                        if let Some(name) = path.file_name() {
+                            if let Some(dir_name) = name.to_str() {
+                                    println!("{}{}", pre, dir_name);
                             }
                         }
                     },
@@ -95,6 +99,10 @@ fn list_boxes() -> Result<(), ZkError> {
 fn use_box(matches: &ArgMatches) -> Result<(), ZkError> {
     if let Some(name) = matches.get_one::<String>("name") {
         let path = utils::path_from_name(&name)?;
+        if is_tracked(&path)? == false {
+            println!("This box has been removed. Use `box track` to add it.");
+            return Ok(());
+        };
         match fs::exists(&path) {
             Ok(true) => {
                 let mut current = utils::get_zk_dir()?;
@@ -129,7 +137,10 @@ fn git_init(path: &PathBuf) -> Result<(), ZkError> {
         .arg("init")
         .arg(path)
         .output() {
-            Ok(_) => Ok(()),
+            Ok(_) => {
+                println!("successfully initialized git for box");
+                Ok(())
+            },
             Err(_) => Err(ZkError::GitInit)
     }
 }
@@ -138,8 +149,18 @@ fn track(path: &PathBuf) -> Result<(), ZkError> {
     utils::verify_path(path)?;
     let mut track_file = PathBuf::from(path);
     track_file.push(".track");
+    match fs::exists(&track_file) {
+        Ok(true) => {
+            println!("box is already tracked");
+            return Ok(());
+        },
+        _ => {}
+    };
     match fs::File::create(track_file) {
-        Ok(_) => Ok(()),
+        Ok(_) => {
+            println!("box successfully tracked");
+            Ok(())
+        },
         Err(_) => Err(ZkError::TrackFail)
     }
 }
@@ -147,22 +168,32 @@ fn track(path: &PathBuf) -> Result<(), ZkError> {
 fn remove_tracking(matches: &ArgMatches) -> Result<(), ZkError> {
     if let Some(name) = matches.get_one::<String>("name") {
         let path = utils::path_from_name(name)?;
-        remove_track_file(&path);
-        return Ok(());
+        remove_track_file(&path)?;
+        Ok(())
+    } else {
+        Err(ZkError::NoName)
     }
-    Err(ZkError::NoName)
-        
 }
 
-fn remove_track_file(path: &PathBuf) {
+fn remove_track_file(path: &PathBuf) -> Result<(), ZkError> {
     match is_tracked(path) {
         Ok(true) => {
             let mut track_file = PathBuf::from(path);
             track_file.push(".track");
-            fs::remove_file(track_file);
+            match fs::remove_file(track_file) {
+                Ok(_) => {
+                    println!("Successfully removed box");
+                    Ok(())
+                },
+                Err(_) => {
+                    Err(ZkError::BoxRemove)
+                }
+            }
         },
-        _ => {}
-    };
+        _ => {
+            Err(ZkError::BoxNotTracked)
+        }
+    }
 }
 
 fn is_tracked(path: &PathBuf) -> Result<bool, ZkError> {
